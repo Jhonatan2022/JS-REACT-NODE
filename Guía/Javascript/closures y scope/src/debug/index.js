a0555500c27f7119e87709b793a470ead2e16c9d
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-08-04 14:31:23 -0500

    Removing Code

---
+++ Guía/Javascript/closures y scope/src/debug/index.js
@@ -0,0 +1,25 @@
+var a = "hello";
+
+function hello() {
+  let b = "hello world";
+  const c = "hello world!";
+  if (true) {
+    let d = "hello world!!";
+    debugger;
+  }
+}
+
+
+// Pal eduard
+var first_number = 13;
+var second_number = 27;
+
+
+function add() {
+    function sum(f1, f2) {
+        return f1 + f2;
+    }
+    console.log(sum(first_number, second_number));        
+}
+
+add(first_number, second_number);
