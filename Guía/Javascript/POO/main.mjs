8cfb72548e6b3ffbdcc9c2d6ecadddc4bf0e77a2
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-08-08 10:28:52 -0500

    Class

---
+++ Guía/Javascript/POO/main.mjs
@@ -0,0 +1,145 @@
+function playVideo(id) {
+    const urlSecreta = "https://platziultrasecretomasquelanasa.com/" + id;
+}
+
+function stopVdeo (id){
+    const urlSecreta = "https://platziultrasecretomasquelanasa.com/" + id;
+}
+
+
+export class PlatziClass {
+    constructor({
+        name,
+        videoID,
+    }) {
+        this.name = name;
+        this.videoID = videoID;
+    }
+
+    reproducir() {
+        playVideo(this.videoID);
+    }
+
+    pausar() {
+        stopVdeo(this.videoID);
+    }
+}
+
+
+class Course {
+    constructor({
+      name,
+      classes = [],
+    }) {
+      this._name = name;
+      this.classes = classes;
+    }
+
+    //Metodo get
+    get name(){
+        return this._name;
+    }
+
+    //Metodo set
+    set name(newName){
+        if (newName === "Curso Malo"){
+            console.error("Web... no");
+        } else {
+            this._name = newName;
+        }
+    }
+  }
+  
+  const cursoProgBasica = new Course({
+    name: "Curso Gratis de Programación Básica",
+  });
+  const cursoDefinitivoHTML = new Course({
+    name: "Curso Definitivo de HTML y CSS",
+  });
+  const cursoPracticoHTML = new Course({
+    name: "Curso Practico de HTML y CSS",
+  });
+  
+  
+  class LearningPath {
+    constructor({
+      name,
+      courses = [],
+    }) {
+      this.name = name;
+      this.courses = courses;
+    }
+  }
+  
+  const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrollo Web",
+    courses: [
+      cursoProgBasica,
+      cursoDefinitivoHTML,
+      cursoPracticoHTML,
+    ],
+  });
+  
+  const escuelaData = new LearningPath({
+    name: "Escuela de Data Science",
+    courses: [
+      cursoProgBasica,
+      "Curso DataBusiness",
+      "Curso Dataviz",
+    ],
+  });
+  
+  const escuelaVgs = new LearningPath({
+    name: "Escuela de Vidweojuegos",
+    courses: [
+      cursoProgBasica,
+      "Curso de Unity",
+      "Curso de Unreal",
+    ],
+  })
+  
+  class Student {
+    constructor({
+      name,
+      email,
+      username,
+      twitter = undefined,
+      instagram = undefined,
+      facebook = undefined,
+      approvedCourses = [],
+      learningPaths = [],
+    }) {
+      this.name = name;
+      this.email = email;
+      this.username = username;
+      this.socialMedia = {
+        twitter,
+        instagram,
+        facebook,
+      };
+      this.approvedCourses = approvedCourses;
+      this.learningPaths = learningPaths;
+    }
+  }
+  
+  const juan2 = new Student({
+    name: "JuanDC",
+    username: "juandc",
+    email: "juanito@juanito.com",
+    twitter: "fjuandc",
+    learningPaths: [
+      escuelaWeb,
+      escuelaVgs,
+    ],
+  });
+  
+  const miguelito2 = new Student({
+    name: "Miguelito",
+    username: "migelitofeliz",
+    email: "miguelito@juanito.com",
+    instagram: "migelito_feliz",
+    learningPaths: [
+      escuelaWeb,
+      escuelaData,
+    ],
+  });
