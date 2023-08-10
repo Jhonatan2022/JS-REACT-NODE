0f723f8b434703eb1d7238e10aed9d72e172eb0c
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-08-10 10:05:16 -0500

    Removing Code

--- Guía/Javascript/POO/Static Object/memory.js
+++ Guía/Javascript/POO/Static Object/memory.js
@@ -1,16 +1,91 @@
-const obj1 = {
-    a: "a",
-    b: "b",
-    c: {
-        d: "d",
-        e: "e"
-    },
+// function isObject(subject) {
+//     return typeof subject == "object";
+// }
 
-    editA (){
-        this.a = "A";
-    }
+// function isArray(subject) {
+//     return Array.isArray(subject);
+// }
+
+// function deepCopy(subject) {
+//     let copySubject;
+
+//     const subjectIsObject = isObject(subject);
+//     const subjectIsArray = isArray(subject);
+
+//     if (subjectIsArray) {
+//         copySubject = [];
+//     } else if (subjectIsObject) {
+//         copySubject = {};    
+//     } else {
+//         return subject;
+//     }
+
+//     for (key in subject) {
+//         const keyIsObject = isObject(subject[key]);
+
+//         if (keyIsObject) {
+//             copySubject[key] = deepCopy(subject[key]);
+//         } else {
+//             if (subjectIsArray) {
+//                 copySubject.push(subject[key]);
+//             } else {
+//                 copySubject[key] = subject[key];
+//             }
+//         }
+//     }
+
+//     return copySubject;
+// }
+
+// const studentBase = {
+//     name: undefined,
+//     email: undefined,
+//     age: undefined,
+//     approvedCourses: undefined,
+//     learningPaths: undefined,
+//     socialMedia: {
+//         twitter: undefined,
+//         instagram: undefined,
+//         facebook: undefined,
+//     },
+// };
+
+// const juan = deepCopy(studentBase);
+// Object.defineProperty(juan, "name", {
+//     value: "Juanito",
+//     configurable: false,
+// });
+
+function requiredParams(param) {
+    throw new Error(param + " es obligatorio");
 }
 
+function createStudent({
+    name = requiredParams("name"),
+    email = requiredParams("email"),
+    age,
+    twitter,
+    instagram,
+    facebook,
+    approvedCourses = [],
+    learningPaths = [],
+}) {
+    return {
+        name,
+        email,
+        age,
+        approvedCourses,
+        learningPaths,
+        socialMedia: {
+            twitter,
+            instagram,
+            facebook,
+        },
+    };
+}
 
-const complexObj = JSON.stringify(obj1);
-const obj3 = JSON.parse(complexObj);
+const juan2 = createStudent({
+    name: "Juanito",
+    age: 17,
+    twitter: "fjuandc",
+});
