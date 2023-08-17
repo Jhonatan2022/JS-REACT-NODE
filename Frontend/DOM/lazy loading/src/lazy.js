16284d48eb920e1c8a529809acdb28f48bdb23fa
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-08-17 08:16:22 -0500

    Removing Code

---
+++ Frontend/DOM/lazy loading/src/lazy.js
@@ -0,0 +1,21 @@
+const isIntersecting = (entry) => {
+    return entry.isIntersecting; // true dentro de la pantalla
+}
+
+const loadImage = (entry) => {
+    const container = entry.target; // <div>
+    const image = container.firstChild;
+    const url = image.dataset.src;
+    // Load image
+    image.src = url;
+    // Unregister
+    observer.unobserve(container);
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(isIntersecting).forEach(loadImage)
+});
+
+export const registerImage = (img) => {
+    observer.observe(img);
+}
